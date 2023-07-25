ce71fcdf96e8734b1792180c8165851811a89442
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-25 17:47:04 +0000

    25 Jul 2023, 23:17

--- validation/index.ts
+++ validation/index.ts
@@ -5,4 +5,10 @@ export const registerSchema = z.object({
   username: z.string().min(3).max(20),
   email: z.string().email(),
   password: z.string().min(6),
+  repeat_password: z
+    .string()
+    .refine((value, data) => value === data.password, {
+      message: 'Passwords do not match',
+    })
+    .transform((value) => value),
 });
