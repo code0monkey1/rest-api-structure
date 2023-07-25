b1cb6403d161733d8ca0339f59ea4a6cdd4f9a05
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-25 23:51:04 +0530

    latest changes

--- validation/index.ts
+++ validation/index.ts
@@ -1,11 +1,19 @@
 // validationSchemas.ts
 import { z } from 'zod';
 
-export const registerSchema = z.object({
-  username: z.string().min(3).max(20),
-  email: z.string().email(),
-  password: z.string().min(6),
-  repeat_password: z.string().min(6),
-});
+export const /* `registerSchema` is a validation schema defined using the `zod` library in TypeScript.
+It defines the structure and validation rules for the data expected during user
+registration. */
+  registerSchema = z
+    .object({
+      username: z.string().min(3).max(20),
+      email: z.string().email(),
+      password: z.string().min(6),
+      repeat_password: z.string().min(6),
+    })
+    .refine((data) => data.password === data.repeat_password, {
+      message: "Passwords don't match",
+      path: ['repeat_password'], // path of error
+    });
 
 registerSchema.refine((obj) => obj.password != obj.repeat_password);
