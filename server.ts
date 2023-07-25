6bb9b72feed326b9fde9931aadf8fcfa3258c3fa
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-26 00:32:04 +0530

    things

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ dotenv.config();
 import { APP_PORT } from './config';
 
 import express from 'express';
+import errorHandler from './middlewares/errorHandler';
 import routes from './src/routes';
 
 const server = express();
@@ -12,6 +13,11 @@ const server = express();
 server.use(express.json());
 
 server.use('/api', routes);
+
+/* `server.use(errorHandler);` is adding the `errorHandler` middleware to the Express server. */
+
+//this is the last middle ware to be added , as it catches all async errors
+server.use(errorHandler);
 
 const PORT = APP_PORT || 3000;
 
