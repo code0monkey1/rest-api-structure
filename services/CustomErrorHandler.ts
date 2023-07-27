35596a53b3f3be1eb1d8313ddb49fd833f89a7b4
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-27 16:56:22 +0530

    latest commit removing the tests

--- services/CustomErrorHandler.ts
+++ services/CustomErrorHandler.ts
@@ -3,41 +3,44 @@ class CustomErrorHandler extends Error {
     super();
   }
 
+  /**
+   * The function returns a new instance of a custom error handler with a status code of 409 and a
+   * given error message.
+   * @param {string} message - The `message` parameter is a string that represents the error message to
+   * be displayed.
+   * @returns A new instance of the CustomErrorHandler class with a status code of 409 and the provided
+   * message.
+   */
   static alreadyExists(message: string) {
-    /* The line `return new CustomErrorHandler(409, message);` is creating a new instance of the
-    `CustomErrorHandler` class with a status code of 409 and a message provided as an argument. It
-    is then returning this new instance of the `CustomErrorHandler` class. */
-
-    /* The number `409` in this context is a status code that represents
-    a conflict error. It indicates that the request could not be
-    completed due to a conflict with the current state of the target
-    resource. */
     return new CustomErrorHandler(409, message);
   }
 
+  /**
+   * The function returns a custom error handler with a 401 status code and a message indicating that
+   * the user authentication failed due to an invalid password or email.
+   * @param [message=password or email invalid] - The message parameter is a string that represents the
+   * error message to be displayed when the user authentication fails. By default, the message is set
+   * to 'password or email invalid'.
+   * @returns A new instance of the CustomErrorHandler class with a status code of 401 and a message of
+   * "password or email invalid".
+   */
   static userAuthFailed(message = 'password or email invalid') {
     /* The line `return new CustomErrorHandler(401, message);` is creating a new instance of the
-    `CustomErrorHandler` class with a status code of 401 and a message of "password or email
-    invalid". It is then returning this new instance of the `CustomErrorHandler` class. */
-
-    /* The number `401` in this context is a status code that represents
-    an unauthorized error. It indicates that the request requires user
-    authentication, but the provided credentials (password or email)
-    are invalid. */
-
+  `CustomErrorHandler` class with a status code of 401 and the provided error message. This instance
+  is then returned by the `wrongUserCredentials` static method. */
     return new CustomErrorHandler(401, message);
   }
 
+  /**
+   * The function returns a custom error handler with a 401 status code and a message indicating
+   * invalid password or email.
+   * @param [message=password or email invalid] - The message parameter is a string that represents the
+   * error message to be displayed when the user credentials are invalid. The default value is
+   * 'password or email invalid'.
+   * @returns a new instance of the CustomErrorHandler class with a status code of 401 and a message of
+   * "password or email invalid".
+   */
   static wrongUserCredentials(message = 'password or email invalid') {
-    /* The line `return new CustomErrorHandler(401, message);` is creating a new instance of the
-    `CustomErrorHandler` class with a status code of 401 and a message of "password or email
-    invalid". It is then returning this new instance of the `CustomErrorHandler` class. */
-
-    /* The number `401` in this context is a status code that represents
-    an unauthorized error. It indicates that the request requires user
-    authentication, but the provided credentials (password or email)
-    are invalid. */
-
     return new CustomErrorHandler(401, message);
   }
 }
