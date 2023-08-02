a7b9041df91748747487b63964fca83b870942b0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-02 17:33:44 +0530

    multer added

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,3 +5,4 @@ export { default as loginController } from './auth/loginController';
 export { default as refreshTokenController } from './auth/refreshTokenController';
 export { default as registerController } from './auth/registerController';
 export { default as userController } from './auth/userController';
+export { default as productController } from './product/productController';
