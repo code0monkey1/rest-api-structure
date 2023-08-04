b1738e93034d18baa11a032baf9083466748a0dc
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-04 22:18:04 +0000

    5 Aug 2023, 03:48

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,6 +1,11 @@
 // validationSchemas.ts
 import { z } from 'zod';
 
+/* The `.refine()` method is used to add additional validation rules to the validation schema. In
+  this case, it is checking if the `password` field is equal to the `repeat_password` field. If
+  they are not equal, it will throw an error with the specified message ("Passwords don't match")
+  and the path of the error will be set to `['repeat_password']`. This allows for more specific
+  error messages and error paths when validating the data. */
 export const registerSchema = z
   .object({
     username: z.string().min(3).max(20),
@@ -8,11 +13,6 @@ export const registerSchema = z
     password: z.string().min(6),
     repeat_password: z.string().min(6),
   })
-  /* The `.refine()` method is used to add additional validation rules to the validation schema. In
-    this case, it is checking if the `password` field is equal to the `repeat_password` field. If
-    they are not equal, it will throw an error with the specified message ("Passwords don't match")
-    and the path of the error will be set to `['repeat_password']`. This allows for more specific
-    error messages and error paths when validating the data. */
   .refine((data) => data.password === data.repeat_password, {
     message: "Passwords don't match",
     path: ['repeat_password'], // path of error
