e3dbe81efc66768865b0d31faf61872ffe3e668b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-12 09:28:59 +0000

    12 Aug 2023, 14:58

---
+++ src/routes/auth-route.ts
@@ -0,0 +1,17 @@
+//Authentication routes
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
+import express from 'express';
+import {
+  loginController,
+  refreshTokenController,
+  registerController,
+} from '../controllers';
+
+const router = express.Router();
+
+router.post('/register', registerController.registerUser);
+
+router.post('/login', loginController.loginUser);
+
+router.post('/refresh', refreshTokenController.refresh);
