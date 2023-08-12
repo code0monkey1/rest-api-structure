b9357fe6771e0d6104ee019d2303c31d7636e71e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-12 10:25:28 +0000

    12 Aug 2023, 15:55

--- src/controllers/auth/registerController.ts
+++ src/controllers/auth/registerController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-
 import { REFRESH_TOKEN_SECRET } from '../../config';
 import { RefreshToken, User } from '../../models';
 import { IUser } from '../../models/UserModel';
@@ -8,28 +7,31 @@ import EncryptionService from '../../services/EncryptionService';
 import JwtService from '../../services/JwtService';
 import { RegisterUserRequest, RegisterUserResponse, Role } from '../../types';
 import { registerSchema } from '../../validation';
+
+type Tokens = {
+  access_token: string;
+  refresh_token: string;
+};
+
 const registerUser = async (
   req: Request<RegisterUserRequest>,
-  res: Response<RegisterUserResponse>
-) => {
+  res: Response<RegisterUserResponse, Tokens>
+): Promise<void> => {
   // [+] validate the request and extract info
   const { username, email, password } = await registerSchema.parseAsync(
     req.body
   );
 
   //[+] check if user is in database already
-
   const userExists = await User.exists({ email });
 
   if (userExists)
     throw CustomErrorHandler.alreadyExists('This email is already taken');
 
   //[+] hash password to encrypt
-
   const hashedPassword = await EncryptionService.getHashedToken(password);
 
-  //[+]create user object
-
+  //[+] create user object
   const userInfo = {
     username,
     email,
@@ -38,18 +40,15 @@ const registerUser = async (
   };
 
   //[+] save user to database
-
   const user: IUser = await User.create(userInfo);
 
   //[+] create access_token
-
   const access_token = await JwtService.sign({
     _id: user._id as string,
     role: user.role,
   });
 
   //[+] create refresh_token
-
   const refresh_token = await JwtService.sign(
     {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -64,8 +63,8 @@ const registerUser = async (
   await RefreshToken.create({ token: refresh_token });
 
   // [+] send response
-
-  res.send({ access_token, refresh_token });
+  const tokens: Tokens = { access_token, refresh_token };
+  res.json(tokens);
 };
 
 export default {
